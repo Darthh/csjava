093b0dc8a4c8ddb5927d7b36059c6fb28e8d126b
Darthh <48933894+Darthh@users.noreply.github.com>
2020-01-24 10:28:27 -0800

    Add files via upload

--- Pokemon.java
+++ Pokemon.java
@@ -1,13 +1,14 @@
 package TestingThings;
 
 public class Pokemon{
-    private int health;
-    private int strength;
+    private int health; //health for all
+    private int strength; //attack
     private int speed; //speed does nothing rn
-    private String name;
+    private String name; //names
     
+    //stats for pokemon
     public Pokemon(String name, int health, int strength, int speed){
-       
+       //var
         this.health = health;
         this.strength = strength;
         this.speed = speed;
@@ -15,36 +16,132 @@ public class Pokemon{
         
     }  
     
-    
-    
-    
+    //BATTLE 1
     public static void battle(Pokemon pokemon1, Pokemon pokemon2)
     {
+    	
+    	//prints pokemon name 1 against 2
         System.out.println(pokemon1.name+" begins the fight against "+pokemon2.name);
         while
         (pokemon1.health >= 1 || pokemon2.health >= 1)
         {
-
+        	//hp - strength + damage to
             pokemon2.health = pokemon2.health - pokemon1.strength;
 
-            System.out.println(pokemon1.name +" does "+ pokemon1.strength +" damage to "+
+            //charizard against blast 1st move
+            System.out.println(pokemon1.name +" uses Inferno and does "+ pokemon1.strength +" damage to "+
                 pokemon2.name +" and "+ pokemon2.name +" has "+ pokemon2.health +" health left.");
 
-            if
+            if //if still has hp then continues
             (pokemon2.health <= 0)
             break;
 
+            // hp - strength + prints out damage to
             pokemon1.health = pokemon1.health - pokemon2.strength;
 
-            System.out.println(pokemon2.name +" does "+ pokemon2.strength +" damage to "+ 
+            //blastoise against charz 1st move 
+            System.out.println(pokemon2.name +" uses Hydro Pump and does "+ pokemon2.strength +" damage to "+ 
                 pokemon1.name +" and "+ pokemon1.name +" has "+ pokemon1.health +" health left.");
 
         }
 
+        //if 1 has less than 0 hp print lose fight
         if
-        (pokemon1.health < 1)
+        (pokemon1.health <= 1)
             System.out.println(pokemon1.name +" has lost the fight");
+        
         else
             System.out.println(pokemon2.name +" has lost the fight");
+        System.out.println("------------------------------------------------------------------");
+        	
     }
+    
+   
+    //BATTLE 2
+    public static void battle2(Pokemon pokemon3, Pokemon pokemon4) //bug with -hp
+    {
+    	
+    	//prints pokemon name 1
+        System.out.println(pokemon3.name+" begins the fight against "+pokemon4.name);
+        while
+        (pokemon3.health >= 1 || pokemon4.health >= 1)
+        {
+        
+            System.out.println(pokemon3.name +" uses SELF DESTRUCT against " + pokemon4.name);
+            
+          //hp - strength + damage to
+            pokemon3.health = pokemon3.health - pokemon3.strength;
+            pokemon4.health = pokemon4.health - pokemon3.strength;
+            
+            System.out.println(pokemon3.name + " has taken " + pokemon3.strength + " damage and now has " + pokemon3.health + "health");
+            System.out.println(pokemon4.name + " has taken " + pokemon3.strength + " damage and now has " + pokemon4.health + "health");
+            
+
+            if
+            (pokemon4.health <= 0)
+            break;
+
+            // hp - strength + prints out damage to
+            pokemon3.health = pokemon3.health - pokemon4.health;
+
+            System.out.println(pokemon4.name +" does "+ pokemon4.strength +" damage to "+ 
+                pokemon3.name +" and "+ pokemon3.name +" has "+ pokemon3.health +" health left.");
+
+        }
+
+        //if 1 has less than 0 hp print lose fight
+        if
+        (pokemon3.health <= 1)
+            System.out.println("Both Pokemon have lost the fight");
+    	
+        else 
+        	 System.out.println(pokemon4.name +" has lost the fight");
+        System.out.println("------------------------------------------------------------------");
+        
+        
+    }
+    //BATTLE 3
+    public static void battle3(Pokemon pokemon5, Pokemon pokemon6) 
+    {
+    	
+    	//prints pokemon name 1
+        System.out.println(pokemon5.name+" begins the fight against "+pokemon6.name);
+        while
+        (pokemon5.health >= 1 || pokemon6.health >= 1)
+        {
+        	//hp - strength + damage to
+            pokemon6.health = pokemon6.health - pokemon5.strength;
+
+            System.out.println(pokemon5.name +" uses Dragon tail and does "+ pokemon5.strength +" damage to "+
+                pokemon6.name +" and "+ pokemon6.name +" has "+ pokemon6.health +" health left.");
+
+            if
+            (pokemon6.health <= 0)
+            break;
+
+            // hp - strength + prints out damage to
+            pokemon5.health = pokemon5.health - pokemon6.strength;
+
+            System.out.println(pokemon6.name +" uses Ice Beam and does "+ pokemon6.strength +" damage to "+ 
+                pokemon5.name +" and "+ pokemon5.name +" has "+ pokemon5.health +" health left.");
+
+        }
+
+        //if 1 has less than 0 hp print lose fight
+        if
+        (pokemon5.health <= 1)
+            System.out.println(pokemon5.name +" has lost the fight");
+    	
+        else 
+        	 System.out.println(pokemon6.name +" has lost the fight");
+        System.out.println("------------------------------------------------------------------");
+        
+        
+           
+    }
+    
+    
+    
+    
+    
 }
--- PokemonTester.java
+++ PokemonTester.java
@@ -1,18 +1,37 @@
 package TestingThings;
 
 public class PokemonTester{
+	//all pokemon
 	  private Pokemon charizard;
 	  private Pokemon blastoise;
+	  private Pokemon wailord;
+	  private Pokemon snorlax;
+	  private Pokemon groudon;
+	  private Pokemon kyogre;
 	 
 	  public PokemonTester(){
-		   charizard = new Pokemon("charizard", 100,50,50);
-		   blastoise = new Pokemon("blastoise", 150,25,150);
+		  //battle 1 poke 1 and 2
+		   charizard = new Pokemon("Charizard", 100,50,50);
+		   blastoise = new Pokemon("Blastoise", 150,25,150);
+		   
+		   //battle 2 poke 3 and 4 bugs with 2nd
+		   wailord = new Pokemon("Wailord", 300, 500, 400);
+		   snorlax = new Pokemon("Snorlax", 200, 300, 200);
+		   
+		   //battle 3 poke 5 and 6
+		   groudon = new Pokemon("Groudon", 250, 100, 100);
+		   kyogre = new Pokemon("Kyogre", 300, 80, 80);
+		   
 		 }
+	  
+	  
+	  
 	  
 	  public static void main(String[] args){
 		    PokemonTester tester=new PokemonTester();
 		    Pokemon.battle(tester.charizard, tester.blastoise);
-		
+		    Pokemon.battle2(tester.wailord, tester.snorlax);
+		    Pokemon.battle3(tester.groudon, tester.kyogre);
 		    
 		    
 	}
