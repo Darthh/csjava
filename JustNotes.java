c88b6616160df1406db79f031ca5e691d0779edf
Darthh <48933894+Darthh@users.noreply.github.com>
2020-02-05 11:23:14 -0800

    Add files via upload

---
+++ JustNotes.java
@@ -0,0 +1,15 @@
+package CoursesProject;
+
+public class JustNotes {
+
+	
+	//https://stackoverflow.com/questions/32232672/filling-a-course-array-with-course-information
+	//https://stackoverflow.com/questions/46270875/wrong-object-is-being-deleted-from-array
+	//https://stackoverflow.com/questions/13558196/how-to-create-a-roster-of-students-in-java
+	//https://stackoverflow.com/questions/31373881/how-do-i-add-students-to-a-roster-from-different-classes-in-java
+	//https://stackoverflow.com/questions/15974486/getters-and-setters-in-java
+	//https://stackoverflow.com/questions/7080542/how-does-a-java-class-get-input
+	//https://stackoverflow.com/questions/33245620/call-a-mutator-method-from-a-separate-class
+	//notes for uml class
+	
+}
