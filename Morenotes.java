769d02dc76b3563c73260ce7fa3ce4e2b26eff90
Darthh <48933894+Darthh@users.noreply.github.com>
2020-03-11 11:34:56 -0700

    Add files via upload

---
+++ Morenotes.java
@@ -0,0 +1,19 @@
+package testing;
+
+public class Morenotes {
+
+	public Morenotes() {
+		//Notes for polymorphism java shapes
+		
+		//http://www.beginwithjava.com/java/inheritance/polymorphism.html
+		//https://github.com/codenuggets-com/codenuggets-com.github.io/blob/d669523fe3c988e7d7dabdf75600f93a3ed33cca/_posts/java-basics/2015-01-18-java-polymorphism.markdown		
+		//https://github.com/beltmankelo/OOP_Java/blob/9589a4b5f754dfec896d7ce312e8c970036f7f03/OOP_LAB/WEEK_6/Shapes.java
+		//https://github.com/ReDI-School/java-intermediate-berlin-2020-spring-website/blob/1e1372a1b621f21d999f40733de3122d41a9e408/docs/previous/fall2019/09-oop-recap-polymorphism.md
+		
+		
+		
+		
+		
+	}
+
+}
