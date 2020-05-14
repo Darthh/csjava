11f85c4a1bb71d2351a1351bd91fad0cfdfcdb41
Darthh <48933894+Darthh@users.noreply.github.com>
2020-05-14 15:49:33 -0700

    Add files via upload

---
+++ Chapter8SummaryHW.java
@@ -0,0 +1,57 @@
+package hw;
+
+public class Chapter8SummaryHW {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Question2. An object is an entity that encapsulates data and behavior that operates on the data.\r\n"+
+		"A class is the blueprint for a type of object, specifying what data and behavior the object will have and how to construct it.");
+		
+		System.out.println();
+		
+		System.out.println("Question4." +
+				 			" 14 14\r\n" + 
+							"7 9 14 2\r\n" + 
+							"18 18\r\n" + 
+							"7 9 14 18");
+		
+		System.out.println();
+		
+		System.out.println("Question6. A field is a variable that exists inside of an object. A parameter is a variable inside a method whose value is passed in from outside.\r\n"+
+				 "Fields have different syntax because they are usually declared with the private keyword and not in a method's header.\r\n"+
+				 "A field's scope is throughout the class, while a parameter's scope is limited to the method.");
+		
+		System.out.println();
+		
+		System.out.println("Question7. public class Name {\r\n" + 
+				"    String firstName;\r\n" + 
+				"    char middleInitial;\r\n" + 
+				"    String lastName;\r\n }");
+		
+		System.out.println();
+		
+		System.out.println("Question 8.\r\n"+ 
+		"An accessor provides the client access to some data in the object, while a mutator lets the client change the object's state in some way.\r\n" +
+		 " Accessors' names often begin with \"get\" or \"is\", while mutators' names often begin with \"set\".");
+		
+		System.out.println();
+		
+		System.out.println("Question 9. see below\r\n"+
+		"	public double distance(Point other) {\r\n"+
+				
+		"		int x1 = other.x;\r\n" +
+		"		int y1 = other.y;\r\n" +
+
+	    " 	return Math.sqrt(Math.pow(this.x * x1, 2) + Math.pow(this.y * y1, 2)); 	}\r\n");
+				
+		System.out.println();
+		
+		System.out.println("Question 14. ");
+		
+		
+		
+		
+	}
+
+}
